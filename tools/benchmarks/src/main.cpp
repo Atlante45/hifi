9f13eb2b401055a3747d1746cef30f3053ced8e3
Clement <clement.brisset@gmail.com>
2018-04-27 15:12:28 -0700

    Random engines

--- tools/benchmarks/src/main.cpp
+++ tools/benchmarks/src/main.cpp
@@ -12,11 +12,12 @@
 
 #include <QSharedPointer>
 #include <memory>
+#include <random>
 
 static void StdPointerCopy(benchmark::State& state) {
     using Pointer = std::shared_ptr<int>;
     Pointer p { new int(0) };
-    
+
     while (state.KeepRunning()) {
         Pointer copy { p };
         benchmark::DoNotOptimize(copy);
@@ -27,7 +28,7 @@ BENCHMARK(StdPointerCopy);
 static void StdPointerRef(benchmark::State& state) {
     using Pointer = std::shared_ptr<int>;
     Pointer p { new int(0) };
-    
+
     while (state.KeepRunning()) {
         Pointer& ref { p };
         benchmark::DoNotOptimize(ref);
@@ -38,7 +39,7 @@ BENCHMARK(StdPointerRef);
 static void QPointerCopy(benchmark::State& state) {
     using Pointer = QSharedPointer<int>;
     Pointer p { new int(0) };
-    
+
     while (state.KeepRunning()) {
         Pointer copy { p };
         benchmark::DoNotOptimize(copy);
@@ -49,12 +50,36 @@ BENCHMARK(QPointerCopy);
 static void QPointerRef(benchmark::State& state) {
     using Pointer = QSharedPointer<int>;
     Pointer p { new int(0) };
-    
+
     while (state.KeepRunning()) {
         Pointer& ref { p };
         benchmark::DoNotOptimize(ref);
     }
 }
 BENCHMARK(QPointerRef);
+
+static void RandomConstruction(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        std::random_device randomDevice;
+        std::mt19937 generator(randomDevice());
+        std::uniform_real_distribution<float> distribution;
+        distribution.reset();
+        auto value = distribution(generator);
+        benchmark::DoNotOptimize(value);
+    }
+}
+BENCHMARK(RandomConstruction);
+
+static void RandomCall(benchmark::State& state) {
+    std::random_device randomDevice;
+    std::mt19937 generator(randomDevice());
+    std::uniform_real_distribution<float> distribution;
+
+    while (state.KeepRunning()) {
+        auto value = distribution(generator);
+        benchmark::DoNotOptimize(value);
+    }
+}
+BENCHMARK(RandomCall);
 
 BENCHMARK_MAIN();
